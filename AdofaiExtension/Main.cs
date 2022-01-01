5465a28e1f4c4b9becfcea4f6cbf23f7364a1413
Hyunsoo, Chung <admin@hyonsu.com>
2022-01-02 01:29:51 +0900

    First release

---
+++ AdofaiExtension/Main.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityModManagerNet;
+
+namespace AdofaiExtension {
+    #if DEBUG
+    [EnableReloading]
+    #endif
+
+    internal static class Main {
+        internal static UnityModManager.ModEntry Mod;
+        private static Harmony _harmony;
+        internal static bool IsEnabled { get; private set; }
+        internal static bool LoadedLevel = false;
+        internal static bool AfterLoadSetting = false;
+        internal static string LevelPath;
+
+        private static void Load(UnityModManager.ModEntry modEntry) {
+            Mod = modEntry;
+            Mod.OnToggle = OnToggle;
+
+            #if DEBUG
+            Mod.OnUnload = Stop;
+            #endif
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value) {
+            IsEnabled = value;
+
+            if (value) Start();
+            else Stop(modEntry);
+
+            return true;
+        }
+        private static void Start() {
+            _harmony = new Harmony(Mod.Info.Id);
+            _harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            var appidPath = $"{Path.GetFullPath(".")}\\steam_appid.txt";
+            if (!File.Exists(appidPath)) {
+                using(StreamWriter sw = File.CreateText(appidPath)) sw.WriteLine("977950");
+                Mod.Logger.Log("created steam_appid.txt");
+            }
+
+            var runnerPath = $"{Path.GetFullPath(".")}\\runner.bat";
+            if (!File.Exists(runnerPath)) {
+                using (StreamWriter sw = File.CreateText(runnerPath)) {
+                    sw.WriteLine($"cd {Path.GetFullPath(".")}");
+                    sw.WriteLine($"start /b \"\" \"{Path.GetFullPath(".")}\\{Application.productName}.exe\" %1");
+                }
+                Mod.Logger.Log("created runner.bat");
+            }
+
+            var result = FileAssociations.SetAssociation(".adofai", "adofai", "A Dance of Fire and Ice level file.",
+                $"{Path.GetFullPath(".")}\\runner.bat", $"{Path.GetFullPath(".")}\\{Application.productName}.exe");
+            Mod.Logger.Log(result ? "registry add success" : "registry add failed");
+        }
+
+        private static bool Stop(UnityModManager.ModEntry modEntry) {
+            _harmony.UnpatchAll(Mod.Info.Id);
+            #if RELEASE
+            _harmony = null;
+            #endif
+            
+            var runnerPath = $"{Path.GetFullPath(".")}\\runner.bat";
+            if(File.Exists(runnerPath)) File.Delete(runnerPath);
+            
+            FileAssociations.RemoveAssociation(".adofai", "adofai");
+
+            return true;
+        }
+
+        public static void OpenLevel(string path = null) {
+            var arguments = Environment.GetCommandLineArgs();
+            if (!LoadedLevel && arguments.Length >= 2) {
+                Mod.Logger.Log("loading clicked file...");
+                
+                path = string.Join(" ", arguments.Skip(1));
+
+                Mod.Logger.Log(path);
+
+                if (!File.Exists(path)) return;
+
+                LevelPath = path;
+
+                SceneManager.LoadScene("scnEditor");
+
+                Mod.Logger.Log("loaded clicked file");
+            }
+            else Mod.Logger.Log("file not clicked");
+        }
+    }
+}
---
+++ AdofaiExtension/MainPatch.cs
@@ -0,0 +1,49 @@
+using ADOFAI;
+using HarmonyLib;
+
+namespace AdofaiExtension.MainPatch {
+    [HarmonyPatch(typeof(scnLevelSelect), "Awake")]
+    internal static class onStart {
+        internal static void Postfix() {
+            Main.OpenLevel();
+        }
+    }
+
+    [HarmonyPatch(typeof(scnEditor), "Start")]
+    internal static class onEditor {
+        internal static void Postfix() {
+            if (Main.LevelPath != null) {
+                Main.Mod.Logger.Log("load editor level");
+
+                // var OpenLevelCo = scnEditor.instance.GetType().GetMethod("OpenLevelCo", BindingFlags.NonPublic | BindingFlags.Instance);
+                // scnEditor.instance.StartCoroutine(OpenLevelCo.Invoke(scnEditor.instance, new object[] {Main.LevelPath}) as IEnumerator);
+                
+                scrController.instance.LoadCustomLevel(Main.LevelPath);
+
+                Main.LevelPath = null;
+                Main.LoadedLevel = true;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(CustomLevel), "Play")]
+    internal static class afterLoad {
+        internal static void Postfix() {
+            if (!Main.AfterLoadSetting) {
+                Main.Mod.Logger.Log("fix editor");
+                Main.AfterLoadSetting = true;
+                // scnEditor.instance.editor.DecideInspectorTabsAtSelected();
+                // scnEditor.instance.settingsPanel.selectedEventType = LevelEventType.SongSettings;
+                scnEditor.instance.settingsPanel.ShowPanel(LevelEventType.SongSettings);
+
+                // var SelectFirstFloor = scnEditor.instance.GetType()
+                //     .GetMethod("SelectFirstFloor", BindingFlags.NonPublic | BindingFlags.Instance);
+                // SelectFirstFloor.Invoke(scnEditor.instance, new object[] {});
+
+                scnEditor.instance.SwitchToEditMode();
+                GCS.customLevelPaths = null;
+                GCS.difficulty = Difficulty.Strict;
+            }
+        }
+    }
+}
